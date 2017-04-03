bde05e5d94a297f4a87cec25fc632fe26bac79e8
avincent6 <vincent6@purdue.edu>
2017-04-03 01:01:13 -0400

    deletes sass and corresponding dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './sass/nav.sass';
+import './css/nav.css';
 
 ReactDOM.render(
   <App />,
